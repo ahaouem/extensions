540a6d5ab98e5035dfcaf7b48f8ff5c043702175
ahaouem <96834553+ahaouem@users.noreply.github.com>
2024-05-05 22:01:48 +0200

    feat: add option to set volume to 0

---
+++ extensions/spotify-controls/src/zeroVolume.ts
@@ -0,0 +1,9 @@
+import {
+  buildScriptEnsuringSpotifyIsRunning,
+  runAppleScriptSilently,
+} from "./utils";
+
+export default async () => {
+  const script = buildScriptEnsuringSpotifyIsRunning(`set sound volume to 0`);
+  await runAppleScriptSilently(script);
+};
